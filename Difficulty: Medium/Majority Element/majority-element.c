f2219c0b410ee69a0dea8018b2398e6183d33efe
Soumi Rathore <rsoumi389@gmail.com>
2026-01-29 15:49:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.c
@@ -0,0 +1,23 @@
+int majorityElement(int arr[], int n) {
+    // code here
+    int c=0,count=0,ans;
+    for(int i=0;i<n;i++){
+        if(c==0){
+            c++;
+            ans=arr[i];
+        }
+        else if(arr[i]==ans)
+            c++;
+        else
+            c--;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==ans)
+            count++;
+    }
+    if(count>(n/2))
+        return ans;
+    else 
+        return -1;
+}
