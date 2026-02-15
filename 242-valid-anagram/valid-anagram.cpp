97215d9f784486f1512afba8d3ab554641bd0b3c
Soumi Rathore <rsoumi389@gmail.com>
2026-02-16 02:10:09 +0530

    Time: 7 ms (19.58%) | Memory: 9.7 MB (74.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,17 +4,12 @@ public:
         if (s.size() != t.size())
             return false;
         map<char, int> m;
-        for (int i = 0; i < s.size(); i++)
-            m[s[i]]++;
+        for (int i : s)
+            m[i]++;
 
-        for (int i = 0; i < t.size(); i++) {
-            if (m.find(t[i]) != m.end())
-                m[t[i]]--;
-            else
+        for (int x : t)
+            if (--m[x] < 0)
                 return false;
-            if (m[t[i]] < 0)
-                return false;
-        }
         return true;
     }
 };
