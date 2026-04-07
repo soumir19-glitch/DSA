c71c092ec3ffa7579a735d1521f3886798064305
Soumi Rathore <rsoumi389@gmail.com>
2026-04-08 03:19:08 +0530

    Time: 3 ms (28.39%) | Memory: 12.6 MB (23.61%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    void bloo(int x, vector<int>& temp, vector<int>& nums, int n,
+    void bloo(int x, vector<int>& temp, vector<int>& nums,
               vector<vector<int>>& ans) {
-        if (x == n) {
+        if (x == nums.size()) {
             ans.push_back(temp);
             return;
         }
-        bloo(x + 1, temp, nums, n, ans);    //dont take
+        bloo(x + 1, temp, nums, ans);    //dont take
         temp.push_back(nums[x]);
-        bloo(x + 1, temp, nums, n, ans);    //take
+        bloo(x + 1, temp, nums, ans);    //take
         temp.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
         vector<vector<int>> ans;
-        int n = nums.size();
-        bloo(0, temp, nums, n, ans);
+        bloo(0, temp, nums, ans);
         return ans;
     }
 };
