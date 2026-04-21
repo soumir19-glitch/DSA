7c370edcb0a4ede517be7c0efed18fb241145fd7
Soumi Rathore <rsoumi389@gmail.com>
2026-04-22 00:28:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.65%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<=1)    return x;
+        int low=1,high=x,mid=0;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(mid==x/mid)
+                return mid;
+            else if(mid>x/mid)
+                high=mid-1;
+            else if(mid<x/mid){
+                low=mid+1;}
+        }
+        return high;
+    }
+};
